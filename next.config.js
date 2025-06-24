318d15cd10af5133b56b60b829137fbfec6a0504
Matt Chapman (Babyfist) <MCbabyfist@gmail.com>
2025-06-24 20:35:07 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Enable static export for web extension packaging
+  output: 'export',
+
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    // Image optimization must be disabled for static exports
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
