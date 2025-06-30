284ca9a43a4ca0d5b1775a53e6e74740cb6c1f59
Matt Chapman (Babyfist) <MCbabyfist@gmail.com>
2025-06-30 05:56:27 +0000

    Great that seemed to fix it but now it is not showing up in a new tab bu

--- public/background.js
+++ public/background.js
@@ -1,13 +1,9 @@
-
+// This script is the background service worker for the extension.
+// It listens for the extension's action button (the icon in the toolbar) to be clicked.
 chrome.action.onClicked.addListener((tab) => {
-  const extensionPageUrl = chrome.runtime.getURL('index.html');
-
-  chrome.tabs.query({ url: extensionPageUrl }, (tabs) => {
-    if (tabs.length > 0) {
-      chrome.tabs.update(tabs[0].id, { active: true });
-      chrome.windows.update(tabs[0].windowId, { focused: true });
-    } else {
-      chrome.tabs.create({ url: extensionPageUrl });
-    }
+  // When the action button is clicked, create a new tab.
+  chrome.tabs.create({
+    // Set the URL of the new tab to the main page of the extension.
+    url: 'index.html'
   });
 });
